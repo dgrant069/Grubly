33fe4c57fa14048c7112f39e2aa3db60795e31f6
SupahNickie <nicholascase@live.com>
2013-10-29 20:52:11 -0700

    Cleaned up past_orders resource to remove unnecessary things

--- app/controllers/past_orders_controller.rb
+++ app/controllers/past_orders_controller.rb
@@ -6,57 +6,9 @@ class PastOrdersController < ApplicationController
     @past_orders = @user.orders.all
   end
 
-  # GET /past_orders/1
-  def show
-  end
-
-  # GET /past_orders/new
-  def new
-    @past_order = PastOrder.new
-  end
-
-  # GET /past_orders/1/edit
-  def edit
-  end
-
-  # POST /past_orders
-  def create
-    @past_order = PastOrder.new(past_order_params)
-
-    if @past_order.save
-      redirect_to @past_order, notice: 'Past order was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /past_orders/1
-  def update
-    if @past_order.update(past_order_params)
-      redirect_to @past_order, notice: 'Past order was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  # DELETE /past_orders/1
-  def destroy
-    @past_order.destroy
-    redirect_to past_orders_url, notice: 'Past order was successfully destroyed.'
-  end
-
   private
+
     def load_user
       @user = User.find(params[:user_id])
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
-#    def set_past_order
- #     @past_order = PastOrder.find(params[:id])
-  #  end
-
-    # Only allow a trusted parameter "white list" through.
-    def past_order_params
-      params[:past_order].permit(:item_id, :quantity, :note, :restaurant_id, :user_id, :user)
     end
 end

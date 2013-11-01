83fd8587e615efb73320dcd5a1750f46331d2144
SupahNickie <nicholascase@live.com>
2013-11-01 10:20:29 -0700

    Updated Restaurant tests

--- test/features/restaurants/delete_order_test.rb
+++ test/features/restaurants/delete_order_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
-feature "A consumer tries to place order and then edit it before payment" do
-  scenario "The consumer can edit order" do
+feature "A consumer tries to place order and then delete it before payment" do
+  scenario "The consumer can delete order" do
     # Given a consumer is signed in and visits the restaurant show page
     sign_in_consumer
     visit new_restaurant_order_path(restaurants(:restaurant1).id)
@@ -13,14 +13,8 @@ feature "A consumer tries to place order and then edit it before payment" do
     click_on "Create Ordered item"
 
     # When they click edit order
-    click_on "Edit Item"
-    # And they select a dish from the order form
-    select(items(:dish2).dish_name)
-    # And enter quantity
-    fill_in "Quantity", with: "5"
-    # And then submit the form
-    click_on "Update Ordered item"
+    click_on "Remove Item"
     # Then consumer should see a confirmation message
-    page.must_have_content "Order was successfully updated"
+    page.must_have_content "Order item was successfully destroyed"
   end
 end
--- test/features/restaurants/new_order_test.rb
+++ test/features/restaurants/new_order_test.rb
@@ -4,21 +4,47 @@ feature "A consumer tries to place orders" do
   scenario "The consumer adds one item to order" do
     # Given a consumer is signed in and visits the restaurant show page
     sign_in_consumer
-    visit restaurant_path(restaurants(:restaurant1).id)
+    visit new_restaurant_order_path(restaurants(:restaurant1).id)
 
     # When they click create order
-    click_on "Place Order"
+    click_on "Create Order"
+    click_on "Show"
+    click_on "Add to Order"
+
     # And they select a dish from the order form
-    select(items(:dish2).dish_name, from: "order_item_to_be_added")
+    select(items(:dish1).dish_name)
+
     # And enter quantity
     fill_in "Quantity", with: "2"
+
     # And then submit the form
-    click_on "Create Order"
+    click_on "Create Ordered item"
+
     # Then consumer should see a confirmation message
-    page.must_have_content "Order was successfully created"
+    page.must_have_content "Order item was successfully added"
+
   end
 
   scenario "The user adds another order" do
-    pending "passing first scenario"
+    # Given that a consumer has an order with one item already in it
+    sign_in_consumer
+    visit new_restaurant_order_path(restaurants(:restaurant1).id)
+    click_on "Create Order"
+    click_on "Show"
+    click_on "Add to Order"
+    select(items(:dish1).dish_name)
+    fill_in "Quantity", with: "2"
+    click_on "Create Ordered item"
+
+    # When they try to add another item to the order
+    click_on "Add to Order"
+    select(items(:dish2).dish_name)
+    fill_in "Quantity", with: "4"
+    click_on "Create Ordered item"
+
+    # The consumer will have more than one item in their order
+    page.text.must_include "Girl scout cookies"
+    page.text.must_include "Anchovie pizza"
+
   end
 end
